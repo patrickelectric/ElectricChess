c32a7be36232a54d0e9d65db947bd479a2cfc295
Patrick <patrickelectric@gmail.com>
2014-02-27 14:54:13 -0300

    adicionado Makefile, e um  código utilizando x11 e opencv

--- Makefile
+++ Makefile
@@ -1,1 +1,23 @@
+# project directories
+PRJDIR 		 := $(shell pwd)
 
+OUTDIR       := $(PRJDIR)/bin
+
+CODDIR       := $(PRJDIR)/code
+LIBDIR       := $(CODDIR)/lib
+SRCDIR       := $(CODDIR)/src
+
+# C source files
+
+#modules
+C_SRC += $(SRCDIR)/main.cpp
+C_LIB += $(LIBDIR)/
+	
+# Define programs and commands.
+CC      = 	gcc
+
+# compiler flags
+CFLAGS  = 	-g -Wall -lX11 -lstdc++ `pkg-config --libs --cflags opencv`
+
+all:
+	$(CC) $^ $(C_SRC) -o $(OUTDIR)/main -I$(C_LIB) $(CFLAGS)
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,1 +1,48 @@
 
+#include "libs.h"
+
+using namespace std;
+using namespace cv;
+
+Mat XImage2MatImageAdapter(XImage *ximage);
+
+int main()
+{
+   Display *display = XOpenDisplay(NULL);
+   Window root = DefaultRootWindow(display);
+
+   XWindowAttributes gwa;
+
+   XGetWindowAttributes(display, root, &gwa);
+   int width = gwa.width;
+   int height = gwa.height;
+
+
+   XImage *image = XGetImage(display,root, 0,0 , width,height,AllPlanes, ZPixmap);
+
+   Mat matImage = XImage2MatImageAdapter(image);
+   
+   imshow("image_show",matImage);
+   waitKey(3000000);
+   
+   return 0;
+}
+
+Mat XImage2MatImageAdapter(XImage *ximage)
+{
+  IplImage *iplImage;
+  assert(ximage->format == ZPixmap);
+  assert(ximage->depth == 24); 
+
+  iplImage = cvCreateImageHeader(
+                cvSize(ximage->width, ximage->height), 
+                IPL_DEPTH_8U,
+                ximage->bits_per_pixel/8);
+
+  iplImage->widthStep = ximage->bytes_per_line;
+  if(ximage->data != NULL)
+    iplImage->imageData = ximage->data;
+	
+  Mat imgmat(iplImage,false);
+  return imgmat;
+};
