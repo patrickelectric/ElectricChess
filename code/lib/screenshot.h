7d5dca3685237d6a8f4c03788000dff062a5c7ce
Patrick <patrickelectric@gmail.com>
2014-02-28 10:00:58 -0300

    add funcao de captura de tela

---
+++ code/lib/screenshot.h
@@ -0,0 +1,47 @@
+/*X11 library*/
+#include <X11/Xlib.h>
+#include <X11/X.h>
+
+/*OpenCV library*/
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+using namespace std;
+using namespace cv;
+
+Mat ScreenShot();
+Mat XImage2MatImageAdapter(XImage *ximage);
+
+Mat ScreenShot()
+{
+  Display *display = XOpenDisplay(NULL);
+   Window root = DefaultRootWindow(display);
+
+   XWindowAttributes gwa;
+
+   XGetWindowAttributes(display, root, &gwa);
+   int width = gwa.width;
+   int height = gwa.height;
+
+
+   return XImage2MatImageAdapter(XGetImage(display,root, 0,0 , width,height,AllPlanes, ZPixmap));  
+}
+
+Mat XImage2MatImageAdapter(XImage *ximage)
+{
+  IplImage *iplImage;
+  assert(ximage->format == ZPixmap);
+  assert(ximage->depth == 24); 
+
+  iplImage = cvCreateImageHeader(
+                cvSize(ximage->width, ximage->height), 
+                IPL_DEPTH_8U,
+                ximage->bits_per_pixel/8);
+
+  iplImage->widthStep = ximage->bytes_per_line;
+  if(ximage->data != NULL)
+    iplImage->imageData = ximage->data;
+	
+  Mat imgmat(iplImage,false);
+  return imgmat;
+};
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,5 +1,15 @@
+/*X11 library*/
+#include <X11/Xlib.h>
+#include <X11/X.h>
 
-#include "libs.h"
+/*OpenCV library*/
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+/*std library*/
+#include <cstdio>
+
+#include "screenshot.h"
 
 using namespace std;
 using namespace cv;
@@ -8,41 +18,9 @@ Mat XImage2MatImageAdapter(XImage *ximage);
 
 int main()
 {
-   Display *display = XOpenDisplay(NULL);
-   Window root = DefaultRootWindow(display);
-
-   XWindowAttributes gwa;
-
-   XGetWindowAttributes(display, root, &gwa);
-   int width = gwa.width;
-   int height = gwa.height;
-
-
-   XImage *image = XGetImage(display,root, 0,0 , width,height,AllPlanes, ZPixmap);
-
-   Mat matImage = XImage2MatImageAdapter(image);
+  Mat matImage = ScreenShot();
+  imshow("image_show",matImage);
+  waitKey(3000000);
    
-   imshow("image_show",matImage);
-   waitKey(3000000);
-   
-   return 0;
+  return 0;
 }
-
-Mat XImage2MatImageAdapter(XImage *ximage)
-{
-  IplImage *iplImage;
-  assert(ximage->format == ZPixmap);
-  assert(ximage->depth == 24); 
-
-  iplImage = cvCreateImageHeader(
-                cvSize(ximage->width, ximage->height), 
-                IPL_DEPTH_8U,
-                ximage->bits_per_pixel/8);
-
-  iplImage->widthStep = ximage->bytes_per_line;
-  if(ximage->data != NULL)
-    iplImage->imageData = ximage->data;
-	
-  Mat imgmat(iplImage,false);
-  return imgmat;
-};
